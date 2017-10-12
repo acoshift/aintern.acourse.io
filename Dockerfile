5942e8db6bbc02a90eb00719a0e066f879812550
acoshift <acoshift@gmail.com>
2017-10-12 23:30:52 +0700

    init

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM acoshift/go-scratch
+
+ADD entrypoint /entrypoint
+RUN mkdir -p dist
+COPY index.html /dist/index.html
+COPY css /dist/css
+COPY js /dist/js
+COPY picture /dist/picture
+EXPOSE 8080
+
+ENTRYPOINT ["/entrypoint"]
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/acoshift/gzip"
+	"github.com/acoshift/middleware"
+	"github.com/acoshift/redirecthttps"
+)
+
+func main() {
+	m := http.NewServeMux()
+	m.Handle("/", http.FileServer(web{"dist"}))
+	m.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	h := middleware.Chain(
+		redirecthttps.New(redirecthttps.Config{Mode: redirecthttps.OnlyProxy}),
+		gzip.New(gzip.DefaultConfig),
+		cacheControl,
+	)(m)
+
+	log.Println("Start Web Sever on :8080")
+	log.Fatal(http.ListenAndServe(":8080", h))
+}
+
+func cacheControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "public, max-age=3600")
+		h.ServeHTTP(w, r)
+	})
+}
+
+type web struct {
+	dir http.Dir
+}
+
+func (w web) Open(name string) (http.File, error) {
+	fs, err := w.dir.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return fs, nil
+}
